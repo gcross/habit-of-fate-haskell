eb58453a141cbdb32e189f999faad3dbe1c57844
Gregory Crosswhite <gcrosswhite@gmail.com>
2016-12-19 14:43:42 -0500

    Now the Server conforms more closely to the JSON API standard

--- sources/library/HabitOfFate/Server.hs
+++ sources/library/HabitOfFate/Server.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
 module HabitOfFate.Server where
 
-import Control.Lens
+import Control.Lens hiding ((.=))
 import Control.Concurrent.MVar
 import Control.Monad.IO.Class
+import Data.Aeson
 import Data.Bool
+import Data.String
 import Data.Text
 import Network.Wai.Handler.Warp
 import Servant
@@ -21,18 +24,39 @@ import HabitOfFate.Behaviors.Habit
 import HabitOfFate.Data
 import HabitOfFate.Unicode
 
-type API = "habits" :> Get '[JSON] [Habit]
+type API = "habits" :> Get '[JSON] Value
 
 api ∷ Proxy API
 api = Proxy
 
 server ∷ MVar Data → Server API
-server mvar = (^. behaviors . habits) <$> liftIO (readMVar mvar)
+server mvar =
+  createSuccessResponse "https://localhost/" ∘ (^. behaviors . habits)
+  <$>
+  liftIO (readMVar mvar)
 
 app ∷ MVar Data → Application
 app mvar = serve api (server mvar)
 
 port = 8081
+
+createSuccessResponse ∷ ToJSON α ⇒ Text → α → Value
+createSuccessResponse url d =
+  object [
+      "links" .= object ["self" .= String url]
+    , "data" .= object [
+          "type" .= String "habits"
+        , "id" .= String ""
+        , "attributes" .= toJSON d
+      ]
+  ]
+
+createFailureResponse ∷ Text → Text → Value
+createFailureResponse url err =
+  object [
+      "links" .= object ["self" .= String url]
+    , "errors" .= err
+  ]
 
 --------------------------------------------------------------------------------
 ------------------------------------- Main -------------------------------------
