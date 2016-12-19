0bcdcb01b2a74aca8e348b9aed98427773d7b653
Gregory Crosswhite <gcrosswhite@gmail.com>
2016-12-19 13:20:29 -0500

    Now the server reads and outputs the data in the data file

--- sources/library/HabitOfFate/Server.hs
+++ sources/library/HabitOfFate/Server.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
 module HabitOfFate.Server where
 
+import Control.Lens
+import Control.Concurrent.MVar
+import Control.Monad.IO.Class
+import Data.Bool
 import Data.Text
 import Network.Wai.Handler.Warp
 import Servant
+import System.Directory
+import System.Log.Logger
+import System.IO
 
+import HabitOfFate.Behaviors
 import HabitOfFate.Behaviors.Habit
+import HabitOfFate.Data
 import HabitOfFate.Unicode
 
 type API = "habits" :> Get '[JSON] [Habit]
@@ -16,14 +26,38 @@ type API = "habits" :> Get '[JSON] [Habit]
 api ∷ Proxy API
 api = Proxy
 
-server ∷ Server API
-server = return [Habit "Test" 1 0]
+server ∷ MVar Data → Server API
+server mvar = (^. behaviors . habits) <$> liftIO (readMVar mvar)
 
-app ∷ Application
-app = serve api server
+app ∷ MVar Data → Application
+app mvar = serve api (server mvar)
 
 port = 8081
 
+--------------------------------------------------------------------------------
+------------------------------------- Main -------------------------------------
+--------------------------------------------------------------------------------
+
 habitMain = do
-  putStrLn $ "Starting server at " ++ show port
-  run port app
+  filepath ← getDataFilePath
+  info $ "Data file is located at " ++ filepath
+  mvar ←
+    doesFileExist filepath
+    >>=
+    bool (do info "Creating new data file"
+             return newData
+         )
+         (do info "Reading existing data file"
+             readData filepath
+         )
+    >>=
+    newMVar
+  notice $ "Starting server at " ++ show port
+  run port (app mvar)
+
+--------------------------------------------------------------------------------
+------------------------------------ Loggers -----------------------------------
+--------------------------------------------------------------------------------
+
+info = infoM "HabitOfFate.Server"
+notice = noticeM "HabitOfFate.Server"
