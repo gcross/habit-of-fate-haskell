1b1fdc5b6c610c56f7520cf9ea92223c5ee27854
Gregory Crosswhite <gcrosswhite@gmail.com>
2016-12-27 12:17:54 -0500

    Tweaked deriveJSON to use underscores instead of spaces.

--- sources/library/HabitOfFate/TH.hs
+++ sources/library/HabitOfFate/TH.hs
@@ -6,8 +6,5 @@ import Data.Aeson.TH (Options(fieldLabelModifier), defaultOptions)
 import qualified Data.Aeson.TH as AesonTH
 
 deriveJSON = AesonTH.deriveJSON $ defaultOptions
-  { fieldLabelModifier =
-      map (\c → if c == '_' then ' ' else c)
-      .
-      (\label → if head label == '_' then tail label else label)
+  { fieldLabelModifier = dropWhile (== '_')
   }
