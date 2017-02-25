69d127c9cb47e22ce833182b6d202dc8a4b2bc93
Gregory Crosswhite <gcrosswhite@gmail.com>
2017-02-25 07:20:17 +0000

    Split off expensive story round-trip tests.

---
+++ habit-of-fate/sources/tests/test-story-roundtrip.hs
@@ -0,0 +1,161 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+module Main where
+
+import HabitOfFate.Prelude hiding (elements)
+
+import Control.Lens.Extras
+import System.Directory
+import System.Log
+import System.Log.Handler.Simple
+import System.Log.Logger
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import qualified Test.Tasty.SmallCheck as S
+import Test.SmallCheck.Series
+
+import HabitOfFate.Story
+
+instance Arbitrary Text where
+  arbitrary = pack <$> arbitrary
+
+instance Arbitrary Color where
+  arbitrary = elements [Red, Green, Blue]
+
+instance Monad m ⇒ Serial m Color where
+
+instance Arbitrary Style where
+  arbitrary = oneof
+    [ return Bold
+    , return Underline
+    , Color <$> arbitrary
+    , return Introduce
+    ]
+
+instance Monad m ⇒ Serial m Style where
+
+instance Arbitrary α ⇒ Arbitrary (GenParagraph α) where
+  arbitrary = sized $ \n →
+    if n <= 1
+      then Text_ <$> arbitrary
+      else oneof
+        [ Style <$> arbitrary <*> resize (n-1) arbitrary
+        , Merged <$> (do
+            nc ← choose (1,n)
+            fromList <$> vectorOf nc (resize (n `div` nc) arbitrary)
+          )
+        , Text_ <$> arbitrary
+        ]
+  shrink (Style _ child) = [child]
+  shrink (Merged children) = toList children
+  shrink (Text_ _) = []
+
+instance Arbitrary α ⇒ Arbitrary (GenEvent α) where
+  arbitrary = GenEvent <$> arbitrary
+  shrink = fmap GenEvent ∘ shrink ∘ unwrapGenEvent
+
+instance Arbitrary α ⇒ Arbitrary (GenQuest α) where
+  arbitrary = GenQuest <$> arbitrary
+  shrink = fmap GenQuest ∘ shrink ∘ unwrapGenQuest
+
+instance Arbitrary α ⇒ Arbitrary (GenStory α) where
+  arbitrary = GenStory <$> arbitrary
+  shrink = fmap GenStory ∘ shrink ∘ unwrapGenStory
+
+instance (Monad m, Serial m α) ⇒ Serial m (Seq α) where
+  series = fromList <$> series
+
+instance Monad m ⇒ Serial m Text where
+  series = pack <$> series
+
+instance Monad m ⇒ Serial m SubText where
+
+instance (Monad m, Serial m α) ⇒ Serial m (GenParagraph α) where
+
+instance (Monad m, Serial m α) ⇒ Serial m (GenEvent α) where
+
+instance (Monad m, Serial m α) ⇒ Serial m (GenQuest α) where
+
+instance (Monad m, Serial m α) ⇒ Serial m (GenStory α) where
+
+log_filename = "test-story-roundtrip.hs"
+
+initialize = do
+  doesFileExist log_filename >>= flip when (removeFile log_filename)
+  file_handler ← fileHandler log_filename DEBUG
+  updateGlobalLogger rootLoggerName $
+    setLevel DEBUG
+    ∘
+    setHandlers [file_handler]
+
+originalFromSubParagraph ∷ SubParagraph → Text
+originalFromSubParagraph =
+  rewords
+  ∘
+  foldMap (
+    \case
+      Literal t → t
+      Key k → "{" ⊕ k ⊕ "}"
+  )
+
+originalFromSubEvent ∷ SubEvent → Text
+originalFromSubEvent =
+  mconcat
+  ∘
+  intersperse "\n"
+  ∘
+  map originalFromSubParagraph
+  ∘
+  unwrapGenEvent
+
+main = initialize >> (defaultMain $ testGroup "All Tests"
+  [ testGroup "HabitOfFate.Story"
+    [ testGroup "round-trip"
+      [ testGroup "Paragraph -> [Node] -> Paragraph" $
+          let doTest story =
+                ( is _Right double_round_trip_xml_text
+                  &&
+                  double_round_trip_xml_text == round_trip_xml_text
+                , message
+                )
+                where
+                  xml_text = renderStoryToText story
+                  round_trip_story = parseStoryFromText xml_text
+                  round_trip_xml_text = renderStoryToText <$> round_trip_story
+                  double_round_trip_story = round_trip_xml_text >>= parseStoryFromText
+                  double_round_trip_xml_text = renderStoryToText <$> double_round_trip_story
+                  message = unlines
+                    ["ORIGINAL STORY:"
+                    ,"    Right " ⊕ show (storyToLists story)
+                    ,"ROUND-TRIP STORY:"
+                    ,"    " ⊕ show (fmap storyToLists round_trip_story)
+                    ,"DOUBLE ROUND-TRIP STORY:"
+                    ,"    " ⊕ show (fmap storyToLists double_round_trip_story)
+                    ,"ORIGINAL XML:"
+                    ,"    Right " ⊕ show xml_text
+                    ,"ROUND-TRIP XML:"
+                    ,"    " ⊕ show round_trip_xml_text
+                    ,"DOUBLE ROUND-TRIP XML:"
+                    ,"    " ⊕ show double_round_trip_xml_text
+                    ]
+          in
+          [ S.testProperty "SmallCheck" $ \story →
+              let (result, message) = doTest story
+              in if result then Right ("" ∷ String) else Left message
+          , localOption (QuickCheckMaxSize 20)
+            $
+            testProperty "QuickCheck" $ \story →
+              let (result, message) = doTest story
+              in counterexample message result
+          ]
+      ]
+    ]
+  ]
+ )
--- habit-of-fate/sources/tests/test.hs
+++ habit-of-fate/sources/tests/test.hs
@@ -25,78 +25,12 @@ import System.Log.Logger
 import System.Random
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck
-import qualified Test.Tasty.SmallCheck as S
-import Test.QuickCheck
-import Test.SmallCheck.Series
 
 import HabitOfFate.Client
 import HabitOfFate.Credits
 import HabitOfFate.Habit
 import HabitOfFate.Server
 import HabitOfFate.Story
-
-instance Arbitrary Text where
-  arbitrary = pack <$> arbitrary
-
-instance Arbitrary Color where
-  arbitrary = elements [Red, Green, Blue]
-
-instance Monad m ⇒ Serial m Color where
-
-instance Arbitrary Style where
-  arbitrary = oneof
-    [ return Bold
-    , return Underline
-    , Color <$> arbitrary
-    , return Introduce
-    ]
-
-instance Monad m ⇒ Serial m Style where
-
-instance Arbitrary α ⇒ Arbitrary (GenParagraph α) where
-  arbitrary = sized $ \n →
-    if n <= 1
-      then Text_ <$> arbitrary
-      else oneof
-        [ Style <$> arbitrary <*> resize (n-1) arbitrary
-        , Merged <$> (do
-            nc ← choose (1,n)
-            fromList <$> vectorOf nc (resize (n `div` nc) arbitrary)
-          )
-        , Text_ <$> arbitrary
-        ]
-  shrink (Style _ child) = [child]
-  shrink (Merged children) = toList children
-  shrink (Text_ _) = []
-
-instance Arbitrary α ⇒ Arbitrary (GenEvent α) where
-  arbitrary = GenEvent <$> arbitrary
-  shrink = fmap GenEvent ∘ shrink ∘ unwrapGenEvent
-
-instance Arbitrary α ⇒ Arbitrary (GenQuest α) where
-  arbitrary = GenQuest <$> arbitrary
-  shrink = fmap GenQuest ∘ shrink ∘ unwrapGenQuest
-
-instance Arbitrary α ⇒ Arbitrary (GenStory α) where
-  arbitrary = GenStory <$> arbitrary
-  shrink = fmap GenStory ∘ shrink ∘ unwrapGenStory
-
-instance (Monad m, Serial m α) ⇒ Serial m (Seq α) where
-  series = fromList <$> series
-
-instance Monad m ⇒ Serial m Text where
-  series = pack <$> series
-
-instance Monad m ⇒ Serial m SubText where
-
-instance (Monad m, Serial m α) ⇒ Serial m (GenParagraph α) where
-
-instance (Monad m, Serial m α) ⇒ Serial m (GenEvent α) where
-
-instance (Monad m, Serial m α) ⇒ Serial m (GenQuest α) where
-
-instance (Monad m, Serial m α) ⇒ Serial m (GenStory α) where
 
 header ∷ String → String
 header header = replicate left_dash_count '-' ⊕ " " ⊕ header ⊕ " " ⊕ replicate right_dash_count '-'
@@ -280,45 +214,6 @@ hopeless task, but {she} has no other choice.
             @?=
             (renderStoryToText $ GenStory [GenQuest [GenEvent [mconcat ["X", " ", "Y"]]]])
         ]
-    , testGroup "round-trip"
-      [ testGroup "Paragraph -> [Node] -> Paragraph" $
-          let doTest story =
-                ( is _Right double_round_trip_xml_text
-                  &&
-                  double_round_trip_xml_text == round_trip_xml_text
-                , message
-                )
-                where
-                  xml_text = renderStoryToText story
-                  round_trip_story = parseStoryFromText xml_text
-                  round_trip_xml_text = renderStoryToText <$> round_trip_story
-                  double_round_trip_story = round_trip_xml_text >>= parseStoryFromText
-                  double_round_trip_xml_text = renderStoryToText <$> double_round_trip_story
-                  message = unlines
-                    ["ORIGINAL STORY:"
-                    ,"    Right " ⊕ show (storyToLists story)
-                    ,"ROUND-TRIP STORY:"
-                    ,"    " ⊕ show (fmap storyToLists round_trip_story)
-                    ,"DOUBLE ROUND-TRIP STORY:"
-                    ,"    " ⊕ show (fmap storyToLists double_round_trip_story)
-                    ,"ORIGINAL XML:"
-                    ,"    Right " ⊕ show xml_text
-                    ,"ROUND-TRIP XML:"
-                    ,"    " ⊕ show round_trip_xml_text
-                    ,"DOUBLE ROUND-TRIP XML:"
-                    ,"    " ⊕ show double_round_trip_xml_text
-                    ]
-          in
-          [ S.testProperty "SmallCheck" $ \story →
-              let (result, message) = doTest story
-              in if result then Right ("" ∷ String) else Left message
-          , localOption (QuickCheckMaxSize 20)
-            $
-            testProperty "QuickCheck" $ \story →
-              let (result, message) = doTest story
-              in counterexample message result
-          ]
-      ]
     ]
   ]
  )
