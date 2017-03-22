06d407159439de30a2d9931716b2ba9114cc1f2f
Gregory Crosswhite <gcrosswhite@gmail.com>
2017-03-21 20:52:38 -0700

    Now logging gets sent to stderr instead of stdout.
    This makes it possible to see the test results without logging junk when
    its not desired.

--- habit-of-fate/sources/library/HabitOfFate/Logging.hs
+++ habit-of-fate/sources/library/HabitOfFate/Logging.hs
@@ -5,8 +5,9 @@ import HabitOfFate.Prelude
 
 import Data.Time.Clock
 import Data.Time.Format
+import System.IO
 
 logIO ∷ MonadIO m ⇒ String → m ()
 logIO message = liftIO $ do
   current_time ← getCurrentTime
-  putStrLn ∘ pack $ formatTime defaultTimeLocale "[%F %X] " current_time ⊕ message
+  hPutStrLn stderr $ formatTime defaultTimeLocale "[%F %X] " current_time ⊕ message
