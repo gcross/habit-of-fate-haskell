1f76d42ede389a2238efd9741f4d5133ea1a1172
Gregory Crosswhite <gcrosswhite@gmail.com>
2017-04-02 09:38:32 -0700

    Now the server exits if one of the certificate files is given but not both.

--- habit-of-fate/sources/library/HabitOfFate/App/Server.hs
+++ habit-of-fate/sources/library/HabitOfFate/App/Server.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
@@ -5,9 +6,11 @@ module HabitOfFate.App.Server where
 
 import HabitOfFate.Prelude
 
+import Control.Lens
 import Network.Wai.Handler.Warp hiding (run)
 import Network.Wai.Handler.WarpTLS
 import Options.Applicative
+import System.Exit
 import System.FilePath
 
 import HabitOfFate.Logging
@@ -15,59 +18,90 @@ import HabitOfFate.Server
 
 import Paths_habit_of_fate
 
-data Configuration = Configuration
-  { port ∷ Int
-  , data_path ∷ FilePath
-  , certificate_path ∷ FilePath
-  , key_path ∷ FilePath
-  , allow_insecure ∷ Bool
-  }
+data CertificateConfiguration =
+    InvalidCertificateConfiguration String
+  | DefaultCertificateConfiguration
+  | CertificateConfiguration
+    { certificate_path ∷ FilePath
+    , key_path ∷ FilePath
+    }
 
-habitMain ∷ IO ()
-habitMain = do
-  default_certificate_path ← getDataFileName $ "data" </> "testing_certificate.pem"
-  default_key_path ← getDataFileName $ "data" </> "testing_key.pem"
-  let default_data_path = "/tmp/habit"
-      configuration_parser ∷ Parser Configuration
-      configuration_parser = Configuration
-        <$> option auto (mconcat
-              [ metavar "PORT"
-              , help "Port to listen on."
-              , long "port"
-              , short 'p'
-              , value 8081
-              ])
-        <*> strOption (mconcat
-              [ metavar "DIRECTORY"
-              , help "Path to game and server data."
-              , long "data"
-              , action "directory"
-              , value default_data_path
-              ])
-        <*> strOption (mconcat
+certificate_configuration_parser ∷ Parser CertificateConfiguration
+certificate_configuration_parser =
+  (
+    (,) <$> (optional ∘ strOption $ mconcat
               [ metavar "FILE"
-              , help "Path to the certificate file."
+              , help "Path to the certificate file. If present, must also specify the key file."
               , long "cert"
               , long "certificate"
               , action "file"
-              , value default_certificate_path
-              ])
-        <*> strOption (mconcat
+              ]
+            )
+        <*> (optional ∘ strOption $ mconcat
               [ metavar "FILE"
-              , help "Path to the key file."
+              , help "Path to the key file. If present, must also specify the certificate file."
               , long "key"
               , action "file"
-              , value default_key_path
-              ])
-        <*> switch (mconcat
-              [ help "Allow insecure connections."
-              , long "allow-insecure"
-              ])
+              ]
+            )
+  )
+  <&>
+  (\case
+    (Nothing, Nothing) →
+      DefaultCertificateConfiguration
+    (Just certificate_path, Just key_path) →
+      CertificateConfiguration certificate_path key_path
+    (Just _, Nothing) →
+      InvalidCertificateConfiguration
+        "Must also specify the key file when specifying the certificate file."
+    (Nothing, Just _) →
+      InvalidCertificateConfiguration
+        "Must also specify the certificate file when specifying the key file."
+  )
+
+data Configuration = Configuration
+  { port ∷ Int
+  , data_path ∷ FilePath
+  , allow_insecure ∷ Bool
+  , certificate_configuration ∷ CertificateConfiguration
+  }
+
+configuration_parser ∷ FilePath → Parser Configuration
+configuration_parser default_data_path = Configuration
+  <$> option auto (mconcat
+        [ metavar "PORT"
+        , help "Port to listen on."
+        , long "port"
+        , short 'p'
+        , value 8081
+        ])
+  <*> strOption (mconcat
+        [ metavar "DIRECTORY"
+        , help "Path to game and server data."
+        , long "data"
+        , action "directory"
+        , value default_data_path
+        ])
+  <*> switch (mconcat
+        [ help "Allow insecure connections."
+        , long "allow-insecure"
+        ])
+  <*> certificate_configuration_parser
+
+habitMain ∷ IO ()
+habitMain = do
   Configuration{..} ←
     execParser $ info
-      (configuration_parser <**> helper)
+      (configuration_parser "/tmp/habit" <**> helper)
       (   fullDesc       <> header "habit-server - server program for habit-of-fate"
       )
+  (certificate_path, key_path) ←
+    case certificate_configuration of
+      InvalidCertificateConfiguration message → putStrLn message >> exitFailure
+      DefaultCertificateConfiguration →
+        (,) <$> getDataFileName ("data" </> "testing_certificate.pem")
+            <*> getDataFileName ("data" </> "testing_key.pem")
+      CertificateConfiguration certificate_path key_path → pure (certificate_path, key_path)
   logIO $ printf "Listening on port %i" port
   logIO $ printf "Using certificate file located at %s" certificate_path
   logIO $ printf "Using key file located at %s" key_path
