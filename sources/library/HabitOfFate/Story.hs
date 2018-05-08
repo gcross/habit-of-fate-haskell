ce783fcd1a9198fe5cd108ec4df2be2eaa39b58f
Gregory Crosswhite <gcrosswhite@gmail.com>
2018-05-08 14:24:14 -0700

    Changed the license to AGPL since it is more fitting for a web server.

--- sources/library/HabitOfFate/Story.hs
+++ sources/library/HabitOfFate/Story.hs
@@ -1,3 +1,19 @@
+{-
+    Habit of Fate, a game to incentivize habit formation.
+    Copyright (C) 2017 Gregory Crosswhite
+
+    This program is free software: you can redistribute it and/or modify
+    it under version 3 of the terms of the GNU Affero General Public License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
+
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveGeneric #-}
