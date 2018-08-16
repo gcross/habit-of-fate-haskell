4f18cbf8dfac02e0dd8ca550d60cd2cc36efe54d
Gregory Crosswhite <gcrosswhite@gmail.com>
2018-08-16 14:32:29 -0400

    Moved the results action code from Server.hs to Actions/Reader.hs

--- sources/library/HabitOfFate/Server.hs
+++ sources/library/HabitOfFate/Server.hs
@@ -83,6 +83,7 @@ import HabitOfFate.Data.Habit
 import HabitOfFate.Logging
 import HabitOfFate.Server.Actions.Common
 import HabitOfFate.Server.Actions.Queries
+import HabitOfFate.Server.Actions.Reader
 import HabitOfFate.Server.Actions.Results
 import HabitOfFate.Story.Renderer.HTML
 import HabitOfFate.Story.Renderer.XML
@@ -117,48 +118,7 @@ import HabitOfFate.Server.Common
 
 ------------------------------------ Reader ------------------------------------
 
-data ReaderInstruction α where
-  ReaderCommonInstruction ∷ CommonInstruction α → ReaderInstruction α
-  ReaderViewInstruction ∷ ReaderInstruction Account
-
-newtype ReaderProgram α = ReaderProgram
-  { unwrapReaderProgram ∷ Program ReaderInstruction α }
-  deriving (Applicative, Functor, Monad)
-
-instance ActionMonad ReaderProgram where
-  singletonCommon = ReaderCommonInstruction >>> Operational.singleton >>> ReaderProgram
-
-instance MonadReader Account (ReaderProgram) where
-  ask = ReaderProgram $ Operational.singleton ReaderViewInstruction
-  local = error "if you see this, then ReaderProgram needs to have a local method"
-
-readerWith ∷ (∀ α. String → ActionM α) → Environment → ReaderProgram ProgramResult → ActionM ()
-readerWith actionWhenAuthFails environment (ReaderProgram program) = do
-  logRequest
-  (username, account_tvar) ← authorizeWith actionWhenAuthFails environment
-  params_ ← params
-  body_ ← Scotty.body
-  account ← account_tvar |> readTVarMonadIO
-  let interpret ∷ ReaderInstruction α → ExceptT Status (Writer (Seq String)) α
-      interpret (ReaderCommonInstruction GetBodyInstruction) = pure body_
-      interpret (ReaderCommonInstruction GetParamsInstruction) = pure params_
-      interpret (ReaderCommonInstruction (RaiseStatusInstruction s)) = throwError s
-      interpret (ReaderCommonInstruction (LogInstruction message)) =
-        void ([i|[#{unwrapUsername username}]: #{message}|] |> singleton |> tell)
-      interpret ReaderViewInstruction = pure account
-      (error_or_result, logs) =
-        program
-          |> interpretWithMonad interpret
-          |> runExceptT
-          |> runWriter
-  traverse_ logIO logs
-  case error_or_result of
-    Left status_ →
-      setStatusAndLog status_
-    Right (ProgramRedirectsTo href) → Scotty.redirect href
-    Right (ProgramResult status_ content) → do
-      setStatusAndLog status_
-      setContent content
+-- import HabitOfFate.Server.Action.Reader
 
 ------------------------------------ Writer ------------------------------------
 
