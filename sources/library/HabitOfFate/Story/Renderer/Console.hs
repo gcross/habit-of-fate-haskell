5c8660844995f67fac68b15375be714095566b05
Gregory Crosswhite <gcrosswhite@gmail.com>
2018-09-20 11:29:36 -0400

    Dropped the render_ prefix in Console.hs

--- sources/library/HabitOfFate/Story/Renderer/Console.hs
+++ sources/library/HabitOfFate/Story/Renderer/Console.hs
@@ -31,11 +31,11 @@ import Rainbow
 import HabitOfFate.Story
 
 data RenderState = RenderState
-  { _render_number_of_columns_ ∷ Int
-  , _render_current_word_ ∷ Seq Char
-  , _render_saw_spaces_last_ ∷ Bool
-  , _render_pending_chunks_ ∷ Seq (Chunk Text)
-  , _render_pending_length_ ∷ Int
+  { _number_of_columns_ ∷ Int
+  , _current_word_ ∷ Seq Char
+  , _saw_spaces_last_ ∷ Bool
+  , _pending_chunks_ ∷ Seq (Chunk Text)
+  , _pending_length_ ∷ Int
   }
 makeLenses ''RenderState
 
@@ -62,11 +62,11 @@ renderStoryToChunks paragraphs =
       go mempty
       >>>
       flip evalStateT (RenderState
-        { _render_number_of_columns_ = 0
-        , _render_current_word_ = mempty
-        , _render_saw_spaces_last_ = False
-        , _render_pending_chunks_ = mempty
-        , _render_pending_length_ = 0
+        { _number_of_columns_ = 0
+        , _current_word_ = mempty
+        , _saw_spaces_last_ = False
+        , _pending_chunks_ = mempty
+        , _pending_length_ = 0
         })
       where
         go formatting (StyleP style rest) = go (addFormat formatting) rest
@@ -84,34 +84,34 @@ renderStoryToChunks paragraphs =
           (forMOf_ text t $ \c →
             if c ∈ " \t\r\n"
               then do
-                saw_spaces_last ← render_saw_spaces_last_ <<.= True
+                saw_spaces_last ← saw_spaces_last_ <<.= True
                 current_word_is_empty ←
                   (&&)
-                    <$> (null <$> use render_current_word_)
-                    <*> (null <$> use render_pending_chunks_)
+                    <$> (null <$> use current_word_)
+                    <*> (null <$> use pending_chunks_)
                 unless (saw_spaces_last || current_word_is_empty) $ do
-                  word ← repack <$> (render_current_word_ <<.= mempty)
-                  word_length ← (olength word +) <$> (render_pending_length_ <<.= 0)
-                  number_of_columns ← use render_number_of_columns_
+                  word ← repack <$> (current_word_ <<.= mempty)
+                  word_length ← (olength word +) <$> (pending_length_ <<.= 0)
+                  number_of_columns ← use number_of_columns_
                   case number_of_columns of
-                    0 → render_number_of_columns_ .= word_length
+                    0 → number_of_columns_ .= word_length
                     _ | number_of_columns + 1 + word_length >= 80 → do
                           tellNewline
-                          render_number_of_columns_ .= word_length
+                          number_of_columns_ .= word_length
                     _ | otherwise → do
                           tellChunk $ chunk " "
-                          render_number_of_columns_ += 1 + word_length
-                  (render_pending_chunks_ <<.= mempty) >>= traverse_ tellChunk
+                          number_of_columns_ += 1 + word_length
+                  (pending_chunks_ <<.= mempty) >>= traverse_ tellChunk
                   tellChunk $ formatting ⊕ chunk word
               else do
-                render_current_word_ %= (⊢ c)
-                render_saw_spaces_last_ .= False
+                current_word_ %= (⊢ c)
+                saw_spaces_last_ .= False
           )
           >>
           (do
-            word ← repack <$> (render_current_word_ <<.= mempty)
-            render_pending_chunks_ %= (⊢ formatting ⊕ chunk word)
-            render_pending_length_ += olength word
+            word ← repack <$> (current_word_ <<.= mempty)
+            pending_chunks_ %= (⊢ formatting ⊕ chunk word)
+            pending_length_ += olength word
           )
         go _ (SubstitutionP sub) = absurd sub
 
