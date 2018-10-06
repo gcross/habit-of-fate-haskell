05486058f8b77d24f97e619b63e94fb0732feee5
Gregory Crosswhite <gcrosswhite@gmail.com>
2018-10-06 16:01:33 -0400

    Now the list of habits uses a CSS grid and spreads out to the whole width.
    # Conflicts:
    #	sources/library/HabitOfFate/Server/Requests/GetAllHabits.hs

--- sources/library/HabitOfFate/Server/Requests/GetAllHabits.hs
+++ sources/library/HabitOfFate/Server/Requests/GetAllHabits.hs
@@ -50,12 +50,29 @@ handleGetAllHabitsWeb environment = do
     quest_status ← ask <&> getAccountStatus
     renderPageAndReturn "Habit of Fate - List of Habits" ["list"] ok200 $ do
       generateTopHTML $ H.div ! A.class_ "story" $ renderEventToHTML quest_status
-      H.div ! A.class_ "list" $ do
-        H.table $ do
-          H.thead $ foldMap (H.toHtml >>> H.th) [""∷Text, "#", "Name", "Difficulty", "Importance", "I succeeded!", "I failed."]
-          H.tbody <<< mconcat $
-            [ H.tr ! A.class_ ("row " ⊕ if n `mod` 2 == 0 then "even" else "odd") $ do
-                H.td $ H.form ! A.method "post" ! A.action (H.toValue $ "/move/" ⊕ show uuid) $ do
+      H.div ! A.class_ "list" $ mconcat $
+        (map (H.toHtml >>> H.div)
+          [ (""∷Text)
+          , ("#")
+          , ("Name")
+          , ("Difficulty")
+          , ("Importance")
+          , ("I succeeded!")
+          , ("I failed.")
+          ]
+        ∷ [H.Html])
+        ⊕
+        concat
+          [ let evenodd = if n `mod` 2 == 0 then "even" else "odd"
+                addScaleElement scale_lens =
+                  H.div ! A.class_ evenodd $ H.toHtml $ displayScale $ habit ^. scale_lens
+                addMarkElement name class_ =
+                  H.div ! A.class_ evenodd $
+                    H.form ! A.method "post" ! A.action (H.toValue $ "/mark/" ⊕ name ⊕ "/" ⊕ show uuid) $
+                      H.input ! A.type_ "submit" ! A.class_ ("smiley " ⊕ class_) ! A.value ""
+            in
+            [ H.div ! A.class_ evenodd $
+                H.form ! A.method "post" ! A.action (H.toValue $ "/move/" ⊕ show uuid) $ do
                   H.input
                     ! A.type_ "submit"
                     ! A.value "Move To"
@@ -64,24 +81,19 @@ handleGetAllHabitsWeb environment = do
                     ! A.value (H.toValue n)
                     ! A.name "new_index"
                     ! A.class_ "new-index"
-                H.td $ H.toHtml (show n ⊕ ".")
-                H.td ! A.class_ "name" $
-                  H.a ! A.href (H.toValue ("/edit/" ⊕ pack (show uuid) ∷ Text)) $ H.toHtml (habit ^. name_)
-                let addScaleElement scale_class scale_lens =
-                      H.td ! A.class_ scale_class $ H.toHtml $ displayScale $ habit ^. scale_lens
-                addScaleElement "difficulty" difficulty_
-                addScaleElement "importance" importance_
-                let addMarkElement name class_ =
-                      H.td $
-                        H.form ! A.method "post" ! A.action (H.toValue $ "/mark/" ⊕ name ⊕ "/" ⊕ show uuid) $
-                          H.input ! A.type_ "submit" ! A.class_ ("smiley " ⊕ class_) ! A.value ""
-                addMarkElement "success" "good"
-                addMarkElement "failure" "bad"
-            | n ← [1∷Int ..]
-            | (uuid, habit) ← habit_list
+            , H.div ! A.class_ evenodd $ H.toHtml (show n ⊕ ".")
+            , H.div ! A.class_ evenodd $
+                H.a ! A.href (H.toValue ("/edit/" ⊕ pack (show uuid) ∷ Text)) $ H.toHtml (habit ^. name_)
+            , addScaleElement difficulty_
+            , addScaleElement importance_
+            , addMarkElement "success" "good"
+            , addMarkElement "failure" "bad"
             ]
-        H.a ! A.class_ "new_link" ! A.href "/new" $ H.toHtml ("New" ∷ Text)
-        H.a ! A.class_ "logout_link" ! A.href "/logout" $ H.toHtml ("Logout" ∷ Text)
+          | n ← [1∷Int ..]
+          | (uuid, habit) ← habit_list
+          ]
+      H.a ! A.class_ "new_link" ! A.href "/new" $ H.toHtml ("New" ∷ Text)
+      H.a ! A.class_ "logout_link" ! A.href "/logout" $ H.toHtml ("Logout" ∷ Text)
 
 handleGetAllHabits ∷ Environment → ScottyM ()
 handleGetAllHabits environment = do
