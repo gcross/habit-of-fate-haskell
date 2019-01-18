4ec3a6f7596d7f2d6ce4b9cb819f7e7064c4e859
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-01-18 15:00:02 -0500

    Deleted some old logging statements in the JavaScript code.

--- data/app/js/edit.js
+++ data/app/js/edit.js
@@ -1,14 +1,9 @@
 function setElementsEnabledByClassName(name, enabled) {
-    console.log("SETTING " + name + " TO " + enabled);
     let elements = document.getElementsByClassName(name + "_control");
-    console.log("ELEMENTS = " + elements);
     for(var i = 0; i < elements.length; i++) {
-        console.log(elements[i]);
         if(elements[i].tagName.toLowerCase() == "input") {
-            console.log("CASE A");
             elements[i].disabled = !enabled;
         } else {
-            console.log("CASE B");
             if(enabled) {
                 elements[i].classList.remove("disabled");
             } else {
@@ -41,17 +36,14 @@ function updateNextDeadlineControlBasedOnOnceHasDeadline() {
 
 function updateEnabled() {
     if(document.getElementById("indefinite_radio").checked) {
-        console.log("CHOSEN INDEFINITE");
         disable("once");
         disable("repeated");
         disable("next_deadline");
     } else if(document.getElementById("once_radio").checked) {
-        console.log("CHOSEN ONCE");
         enable("once");
         disable("repeated");
         updateNextDeadlineControlBasedOnOnceHasDeadline();
     } else {
-        console.log("CHOSEN REPEATED");
         disable("once");
         enable("repeated");
         updateRepeated();
