fa50d1f23ba3580f0f369cf9cfeb84252d059d34
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-01-30 19:12:32 -0500

    Now the template includes AutoDeriveTypeable.

--- misc/Template.hs
+++ misc/Template.hs
@@ -14,6 +14,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -}
 
+{-# LANGUAGE AutoDeriveTypeable #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
