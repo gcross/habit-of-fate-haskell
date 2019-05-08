6a12858dce5be271d8e3415b336fb1144d3e4ad6
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-05-08 17:15:15 -0400

    Got rid of the outer underscores since I probably won't be using lens.

--- sources/library/HabitOfFate/StoryLine.hs
+++ sources/library/HabitOfFate/StoryLine.hs
@@ -27,54 +27,54 @@ import HabitOfFate.Story
 import HabitOfFate.Substitution
 
 data StoryLineOutcome content = StoryLineOutcome
-  { _outcome_text_ ∷ content
-  , _outcome_title_ ∷ Text
-  , _outcome_substitutions_ ∷ [(Text, Gendered)]
+  { outcome_text ∷ content
+  , outcome_title ∷ Text
+  , outcome_substitutions ∷ [(Text, Gendered)]
   }
 
 data StoryQuestion content = StoryQuestion
-  { _question_story_ ∷ content
-  , _question_outcomes_ ∷ Tagged (StoryLineOutcome content)
+  { question_story ∷ content
+  , question_outcomes ∷ Tagged (StoryLineOutcome content)
   }
 
 data StoryEntry content =
     StoryEvent
-      { _event_name_ ∷ Text
-      , _event_title_ ∷ Text
-      , _event_outcomes_ ∷ StoryOutcomes content
-      , _event_initial_question ∷ StoryQuestion content
-      , _event_failure_question ∷ StoryQuestion content
-      , _event_random_stories ∷ [content]
+      { event_name ∷ Text
+      , event_title ∷ Text
+      , event_outcomes ∷ StoryOutcomes content
+      , event_initial_question ∷ StoryQuestion content
+      , event_failure_question ∷ StoryQuestion content
+      , event_random_stories ∷ [content]
       }
   | StoryNarrative
-      { _narrative_name ∷ Text
-      , _narrative_title ∷ Text
-      , _narrative_content ∷ content
+      { narrative_name ∷ Text
+      , narrative_title ∷ Text
+      , narrative_content ∷ content
       }
   | StoryLine
-      { _story_line_name ∷ Text
-      , _story_line_content ∷ [StoryEntry content]
+      { story_line_name ∷ Text
+      , story_line_content ∷ [StoryEntry content]
       }
   | StoryShuffle
-      { _story_lines_to_shuffle_ ∷ [StoryEntry content]
+      { story_lines_to_shuffle ∷ [StoryEntry content]
       }
   | StorySplit
-      { _split_name_ ∷ Text
-      , _split_title_ ∷ Text
-      , _split_story ∷ content
-      , _split_question ∷ Text
-      , _split_branches_ ∷ [StoryBranch content]
+      { split_name ∷ Text
+      , split_title ∷ Text
+      , split_story ∷ content
+      , split_question ∷ Text
+      , split_branches ∷ [StoryBranch content]
       }
 
 data StoryBranch content = StoryBranch
-  { _branch_text_ ∷ Text
-  , _branch_story_entry_ ∷ StoryEntry content
+  { branch_text ∷ Text
+  , branch_story_entry ∷ StoryEntry content
   }
 
 data RandomSubstitution = AnyPerson | AnyMan | AnyWoman | AnyFrom [Gendered]
 
 data QuestStory content = QuestStory
-  { _quest_name_ ∷ Text
-  , _quest_random_substitutions_ ∷ [(Text, RandomSubstitution)]
-  , _quest_entry_ ∷ StoryEntry content
+  { quest_name ∷ Text
+  , quest_random_substitutions ∷ [(Text, RandomSubstitution)]
+  , quest_entryr∷ StoryEntry content
   }
