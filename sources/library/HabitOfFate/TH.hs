fd8442d4e141778d85a82063160318f5e2fd66f8
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-07-15 11:50:08 -0400

    Dropped deriveJSON and friends as it is no longer used.
    This was done with the aim of improving the stability of the JSON encoding
    by having it be made explicit everywhere, which forces me to think about
    how to make it nice.

--- sources/library/HabitOfFate/TH.hs
+++ sources/library/HabitOfFate/TH.hs
@@ -23,20 +23,10 @@ module HabitOfFate.TH where
 
 import HabitOfFate.Prelude
 
-import Data.Aeson.TH (Options(fieldLabelModifier), defaultOptions)
-import qualified Data.Aeson.TH as AesonTH
 import qualified Data.ByteString.Char8 as BS8
 import Instances.TH.Lift ()
 import qualified Language.Haskell.TH.Lift as Lift
 import Language.Haskell.TH.Quote
-
-deriveJSON = AesonTH.deriveJSON $ defaultOptions
-  { fieldLabelModifier = dropWhile (== '_')
-  }
-
-deriveJSONDropping n = AesonTH.deriveJSON $ defaultOptions
-  { fieldLabelModifier = drop n
-  }
 
 textChar8 = QuasiQuoter
   (BS8.pack >>> Lift.lift)
