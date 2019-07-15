dc74babcd84601703b3d51b944f06dd110331fa8
Gregory Crosswhite <gcrosswhite@gmail.com>
2019-07-15 10:12:02 -0400

    Added script center-section.hs to the repository.

---
+++ misc/center-section.hs
@@ -0,0 +1,13 @@
+#!/usr/bin/env runhaskell
+module Main where
+
+import Data.Functor
+import Data.List
+import System.Environment
+
+main = do
+  header <- unwords <$> getArgs
+  let dash_count = 80 - 2 - length header
+      right_dash_count = dash_count `div` 2
+      left_dash_count = dash_count - right_dash_count
+  putStrLn $ replicate left_dash_count '=' ++ " " ++ header ++ " " ++ replicate right_dash_count '='
