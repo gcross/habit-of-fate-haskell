6d4476854a5476208af74766cae790f66bfc0c09
Gregory Crosswhite <gcrosswhite@gmail.com>
2020-04-02 14:24:45 -0400

    Now _polish.sh no longer wraps.
    The problem is that it was only inserting line breaks, no rewrapping
    paragraphs like I wanted it to.

--- misc/_polish.sh
+++ misc/_polish.sh
@@ -4,5 +4,5 @@ sed -i 's/“/"/g' $1
 sed -i 's/“/"/g' $1
 sed -i 's/”/"/g' $1
 sed -i 's/…/.../g' $1
-fold -s -w 80 "$1" > "$1.wrapped"
-mv "$1.wrapped" "$1"
+#fold -s -w 80 "$1" > "$1.wrapped"
+#mv "$1.wrapped" "$1"
